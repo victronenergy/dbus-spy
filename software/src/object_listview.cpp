7f78a4f02cb880dc35b51be687aa8443f5198a97
Jeroen Hofstee <jhofstee@victronenergy.com>
2025-12-11 13:02:20 +0100

    switch from type to typeId
    type is being deprecated in Qt.

--- software/src/object_listview.cpp
+++ software/src/object_listview.cpp
@@ -157,14 +157,14 @@ QString ObjectListView::convertVariant(const QVariant &value)
 		QDBusVariant v = qvariant_cast<QDBusVariant>(value);
 		return convertVariant(v.variant());
 	}
-	switch (value.type()) {
-	case QVariant::Double:
+	switch (value.typeId()) {
+	case QMetaType::Double:
 		return QString::number(value.toDouble());
-	case QVariant::Invalid:
+	case QMetaType::UnknownType:
 		return "-";
-	case QVariant::StringList:
+	case QMetaType::QStringList:
 		return "[" + value.toStringList().join(",") + "]";
-	case QVariant::List:
+	case QMetaType::QVariantList:
 	{
 		QList<QVariant> list = value.toList();
 		QString r = "[";
@@ -178,7 +178,7 @@ QString ObjectListView::convertVariant(const QVariant &value)
 		r.append("]");
 		return r;
 	}
-	case QVariant::Map:
+	case QMetaType::QVariantMap:
 	{
 		QMap<QString, QVariant> map = value.toMap();
 		QString r = "{";
